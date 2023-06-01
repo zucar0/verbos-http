349430b56d0d4e2729b35713d9016bc3b17bdb36
Antonio Enríquez <42826452+zucar0@users.noreply.github.com>
2023-06-01 21:52:33 +0000

    hasta metodo GET y POST

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+// Se va a mostrar que el servidor ha iniciado en el puerto 3000
+app.listen(4000, () =>{
+    console.log('Servidor en puerto 4000')
+});
+
+
+//Método GET
+app.get('/', (req, res) =>{
+    res.send('Recibido.')
+});
+
+//Método POST
+app.post('/', (req, res)=>{
+    res.send('Guardando.')
+});
